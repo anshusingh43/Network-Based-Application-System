e2b04b088ba18d5ca70cd49ff3b66e4c393024cf
Anshu Singh <anshusingh7091994@gmail.com>
2019-09-27 16:35:07 -0400

    Add files via upload

---
+++ Second Phase Development/model/ItemDB.js
@@ -0,0 +1,152 @@
+var Paneer_Veg_Sauted =
+{
+  item_code : "B1",
+  Name : "Paneer Veg Sauted",
+  Catalog_Category : "Breakfast",
+  Description : "Paneer Veg Sauted",
+  Rating : "../assets/images/fiveStar.png",
+  ImageURL : "../assets/images/paneerChilly.jpg"
+}
+var Chicken_Rice =
+{
+  item_code : "L1",
+  Name : "Chicken Rice",
+  Catalog_Category : "Lunch",
+  Description : "Chicken Rice",
+  Rating : "../assets/images/fiveStar.png",
+  ImageURL : "../assets/images/Chicken_Rice.jpg"
+}
+var Lentil_Soup =
+{
+  item_code : "D1",
+  Name : "Lentil Soup",
+  Catalog_Category : "Dinner",
+  Description : "Lentil Soup",
+  Rating : "../assets/images/threeRating.png",
+  ImageURL : "../assets/images/Lentil_Soup.jpg"
+}
+var Avocado_Toast =
+{
+  item_code : "B2",
+  Name : "Avocado Toast",
+  Catalog_Category : "Breakfast",
+  Description : "Avocado Toast",
+  Rating : "../assets/images/fiveStar.png",
+  ImageURL : "../assets/images/avocado_toast.jpeg"
+}
+var Sauted_Fish =
+{
+  item_code : "L2",
+  Name : "Sauted Fish",
+  Catalog_Category : "Lunch",
+  Description : "Sauted Fish",
+  Rating : "../assets/images/fiveStar.png",
+  ImageURL : "../assets/images/Sauted_Fish.jpg"
+}
+var Salted_Soyaoats =
+{
+  item_code : "D2",
+  Name : "Salted Oats And Soyabean",
+  Catalog_Category : "Dinner",
+  Description : "Salted Soyaoats",
+  Rating : "../assets/images/threeRating.png",
+  ImageURL : "../assets/images/Salted_oats_and_soyabean.jpeg"
+}
+var Oats_Twisted =
+{
+  item_code : "B3",
+  Name : "Oats Twisted",
+  Catalog_Category : "Breakfast",
+  Description : "Oats Twisted",
+  Rating : "../assets/images/fiveStar.png",
+  ImageURL : "../assets/images/Oats_twisted.jpg"
+}
+var Veg_Bowl_With_Humus =
+{
+  item_code : "L3",
+  Name : 'Veg Bowl With Humus',
+  Catalog_Category : "Lunch",
+  Description : "Veg Bowl With Humus",
+  Rating : "../assets/images/fiveStar.png",
+  ImageURL : "../assets/images/Veg_bowl_with_humus.jpg"
+}
+var Budda_Bowl =
+{
+  item_code : "D3",
+  Name : "Budda Bowl",
+  Catalog_Category : "Dinner",
+  Description : "Budda Bowl",
+  Rating : "../assets/images/threeRating.png",
+  ImageURL : "../assets/images/Budda_Bowl.jpg"
+}
+var Beery_Merry =
+{
+  item_code : "B4",
+  Name : "Beery Merry",
+  Catalog_Category : "Breakfast",
+  Description : "Beery Merry",
+  Rating : "../assets/images/fiveStar.png",
+  ImageURL : "../assets/images/Beery_Merry.jpg"
+}
+var Mushroom_Chicken =
+{
+  item_code : "L4",
+  Name : "Mushroom Chicken",
+  Catalog_Category : "Lunch",
+  Description : "Mushroom Chicken",
+  Rating : "../assets/images/fiveStar.png",
+  ImageURL : "../assets/images/Mushroom_Chicken.jpg"
+}
+var Egg_white_Salad =
+{
+  item_code : "D4",
+  Name : "egg whites and salad",
+  Catalog_Category : "Dinner",
+  Description : "egg whites and salad",
+  Rating : "../assets/images/threeRating.png",
+  ImageURL : "../assets/images/egg_whites_and_salad.jpg"
+}
+var Chicken_Tikka =
+{
+  item_code : "D5",
+  Name : "Chicken Tikka",
+  Catalog_Category : "Dinner",
+  Description : "Chicken Tikka",
+  Rating : "../assets/images/threeRating.png",
+  ImageURL : "../assets/images/Chicken_Tikka.jpg"
+}
+var Eggitarian_Twist =
+{
+  item_code : "B5",
+  Name : "Eggitarian Twist",
+  Catalog_Category : "Breakfast",
+  Description : "Eggitarian Twist",
+  Rating : "../assets/images/fiveStar.png",
+  ImageURL : "../assets/images/Eggitarian_Twist.jpg"
+}
+var SalsaVeg_Burito =
+ {
+  item_code : "L5",
+  Name : "SalsaVeg Burito",
+  Catalog_Category : "Lunch",
+  Description : "SalsaVeg Burito",
+  Rating : "../assets/images/fiveStar.png",
+  ImageURL : "../assets/images/Salsa_Veg_Burito.jpg"
+}
+
+
+module.exports.Paneer_Veg_Sauted = Paneer_Veg_Sauted;
+module.exports.Chicken_Rice = Chicken_Rice;
+module.exports.Lentil_Soup = Lentil_Soup;
+module.exports.Avocado_Toast = Avocado_Toast;
+module.exports.Sauted_Fish = Sauted_Fish;
+module.exports.Salted_Soyaoats = Salted_Soyaoats;
+module.exports.Oats_Twisted = Oats_Twisted;
+module.exports.Veg_Bowl_With_Humus = Veg_Bowl_With_Humus;
+module.exports.Budda_Bowl = Budda_Bowl;
+module.exports.Beery_Merry = Beery_Merry;
+module.exports.Mushroom_Chicken = Mushroom_Chicken;
+module.exports.Egg_white_Salad = Egg_white_Salad;
+module.exports.Eggitarian_Twist = Eggitarian_Twist;
+module.exports.Chicken_Tikka = Chicken_Tikka;
+module.exports.SalsaVeg_Burito = SalsaVeg_Burito;
